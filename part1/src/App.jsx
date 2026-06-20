bcd13a392a9e4f6e2fd2d7d0bb08500449b555ec
Cyrus Speicher <cyrus.j.speicher@gmail.com>
2026-06-20 17:35:32 -0400

    part1 exercise 1.6, App.jsx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,27 +1,47 @@
-import {useState} from 'react'
-
-const Display = props => <div>{props.value}</div>
+import { useState } from 'react'
 
 const Button = (props) => (
-    <button onClick={props.onClick}>
-      {props.text}
-    </button>
+  <button onClick={props.onClick}>{props.text}</button>
+)
+
+const Display = (props) => (
+  <div>
+    <p>good {props.good}</p>
+    <p>neutral {props.neutral}</p>
+    <p>bad {props.bad}</p>
+  </div>
 )
 
 const App = () => {
-  const [value, setValue] = useState(10)
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const setToValue = newValue => {
-    console.log('value now', newValue) //print the new value to the console
-    setValue(newValue)
+  const addGood = newGood => {
+    console.log('good', newGood)
+    setGood(newGood)
   }
-  
+
+  const addNeutral = newNeutral => {
+    console.log('neutral', newNeutral)
+    setNeutral(newNeutral)
+  }
+
+  const addBad = newBad => {
+    console.log('bad', newBad)
+    setBad(newBad)
+  }
+
   return (
     <div>
-      <Display value={value} />
-      <Button onClick={() => setToValue(1000)} text='thousand' />
-      <Button onClick={() => setToValue(0)} text='reset' />
-      <Button onClick={() => setToValue(value + 1)} text='increment' />
+      <h1>give feedback</h1>
+      <Button onClick={() => addGood(good + 1)} text='good' />
+      <Button onClick={() => addNeutral(neutral + 1)} text='neutral' />
+      <Button onClick={() => addBad(bad + 1)} text='bad' />
+
+      <h1>statistics</h1>
+      <Display good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
