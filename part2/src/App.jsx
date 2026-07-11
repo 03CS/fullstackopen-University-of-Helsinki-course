b13de4479fe89d64da6ec640e91a10f5bfc72738
Cyrus Speicher <cyrus.j.speicher@gmail.com>
2026-07-11 13:31:54 -0400

    part2 exercise 2.1

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,15 +1,54 @@
-import Note from './components/Note'
+const Header = (props) => <h1>{props.course}</h1>
 
-const App = ({ notes }) => {
+const Content = (props) => {
+  const {course} = props
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
+        {course.map(part => <Part key={part.id} part={part} />)}
     </div>
+  )
+}
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => <p>Number of exercises {props.total}</p>
+
+const Course = (props) => (
+  <div>
+    <Header course={props.course.name} />
+    <Content course={props.course.parts} />
+  </div>
+)
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+    ],
+  }
+
+  return (
+    <Course course={course} />
   )
 }
 
